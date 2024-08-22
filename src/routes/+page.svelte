f80c2d0391bc95538354ef5f6f077f9c5e86c24d
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2024-08-22 12:44:12 +0100

    Landing page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // Any specific script logic for the page
+</script>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Calistoga&display=swap");
+
+    .calistoga-regular {
+        font-family: "Calistoga", serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+    .section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        text-align: center;
+        flex-direction: column;
+    }
+
+    .landingbg {
+        background-image: url(../assets/owl2.jpg);
+        background-size: cover;
+        background-position: center;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    h1 {
+        font-size: 2.5rem;
+        margin: 1rem 0;
+    }
+
+    p {
+        font-size: 1.25rem;
+        margin: 1rem 0;
+    }
+
+    a {
+        color: #ff3e00;
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+
+    .title {
+        font-size: 9rem;
+        color: #fae6cb;
+        font-family: "Calistoga", serif;
+        text-shadow: 2px 2px 4px #000000;
+        margin-top: 500px;
+    }
+    .subtitle {
+        font-size: 20px;
+        color: #fae6cb;
+        font-family: "Calistoga", serif;
+        text-shadow: 2px 2px 4px #000000;
+        max-width: 900px;
+    }
+</style>
+
+<div class="section landingbg">
+    <h1 class="title">The Three Owls</h1>
+    <div class="subtitle">
+        The owls that watch over Leeds are not just symbolic; they are deeply woven into the city’s cultural and historical fabric, with three prominent locations showcasing these guardians. The most iconic is the Golden Owl perched on Leeds Civic Hall, overlooking Millennium Square, a creation by John Thorp in 2000 that symbolizes the city’s wisdom and vigilance. Another pair of owls can be found at Owl Corner, near Leeds Town Hall and The Light shopping center, continuing the tradition of these wise sentinels. The third location is the Owl on the Central Library, a majestic figure that has stood for decades, representing knowledge and learning. These owls, rooted in the city’s coat-of-arms and the legacy of the Savile family, have watched over Leeds since the family was granted lands here after the Battle of Hastings in 1066. As enduring symbols of the city’s heritage, these owls embody the resilience, wisdom, and steadfastness that have guided Leeds through centuries of change.
+    </div>
+</div>
